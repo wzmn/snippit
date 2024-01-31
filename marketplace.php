6da5f80724e875f99f310696e45008257a9ec944
Wiseman Fernandes <wizyman@gmail.com>
2024-01-31 12:11:30 +0530

    uncommmented the submit button -_-

--- marketplace.php
+++ marketplace.php
@@ -885,7 +885,7 @@ if (have_posts()):
                                             </label>
                                         </div>
 
-                                        <!-- <div class="relative">
+                                        <div class="relative">
                                             <input class="bg-blue p-2 text-white w-full rounded-[0.375rem]" type="submit"
                                                 value="Sign Up">
                                             <div class="loader">
@@ -894,7 +894,7 @@ if (have_posts()):
                                                         d="M304 48c0-26.5-21.5-48-48-48s-48 21.5-48 48s21.5 48 48 48s48-21.5 48-48zm0 416c0-26.5-21.5-48-48-48s-48 21.5-48 48s21.5 48 48 48s48-21.5 48-48zM48 304c26.5 0 48-21.5 48-48s-21.5-48-48-48s-48 21.5-48 48s21.5 48 48 48zm464-48c0-26.5-21.5-48-48-48s-48 21.5-48 48s21.5 48 48 48s48-21.5 48-48zM142.9 437c18.7-18.7 18.7-49.1 0-67.9s-49.1-18.7-67.9 0s-18.7 49.1 0 67.9s49.1 18.7 67.9 0zm0-294.2c18.7-18.7 18.7-49.1 0-67.9S93.7 56.2 75 75s-18.7 49.1 0 67.9s49.1 18.7 67.9 0zM369.1 437c18.7 18.7 49.1 18.7 67.9 0s18.7-49.1 0-67.9s-49.1-18.7-67.9 0s-18.7 49.1 0 67.9z" />
                                                 </svg>
                                             </div>
-                                        </div> -->
+                                        </div>
                                         <div class="error-message text-center py-4">
                                             <div class="messages"></div>
                                         </div>
@@ -923,9 +923,9 @@ if (have_posts()):
                     
                 document.addEventListener("DOMContentLoaded", function() {
                     const formState = {
-                        otpApi: 'https://prod-eks.snippit.com.au/api/core/otp',
-                        otpVerifyApi: 'https://prod-eks.snippit.com.au/api/core/otp/verify-otp',
-                        registerApi: 'https://prod-eks.snippit.com.au/api/company/basic/',
+                        otpApi: 'https://services.snippit.com.au/api/core/otp',
+                        otpVerifyApi: 'https://services.snippit.com.au/api/core/otp/verify-otp',
+                        registerApi: 'https://services.snippit.com.au/api/company/basic/',
                         emailOtpSent: !1,
                         mobileOtpSent: !1,
                         resendCount: 3,
