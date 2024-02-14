9eef56355b73e151c016876b4501b7858655b422
ankita-cogncise <ankitak@cogncise.com>
2024-02-14 08:42:02 +0530

    site conversion google tag

--- header.php
+++ header.php
@@ -52,6 +52,15 @@
 			});
 		})();
 	</script>
+	<!-- Google tag (gtag.js) -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=AW-11483890180"></script>
+	<script>
+	window.dataLayer = window.dataLayer || [];
+	function gtag(){dataLayer.push(arguments);}
+	gtag('js', new Date());
+
+	gtag('config', 'AW-11483890180');
+	</script>
 </head>
 <body <?php body_class(); ?>>
 	<?php wp_body_open(); ?>
